36ec9cb233062a7ea01094b26cfee21a66ffe43d
Hannah <HannahCheyenne@gmail.com>
2020-09-14 19:48:01 -0500

    preparing for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/blogful-auth',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/capstone1',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/capstone1-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
--- test/setup.js
+++ test/setup.js
@@ -4,9 +4,6 @@ process.env.JWT_SECRET = 'test-jwt-secret'
 
 require('dotenv').config()
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder_mifflin@localhost/blogful-auth-test"
-
 const { expect } = require('chai')
 const supertest = require('supertest')
 
