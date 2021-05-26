72e759bb37ec84c21853870ff801418dc036becd
Hannah <HannahCheyenne@gmail.com>
2021-05-25 20:11:52 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:8000",
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/capstone1',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
